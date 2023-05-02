33b7fad7d5b5d6fa00c5d6d6cb2c4757ce2e9c0e
naanahi <ryuu_yoshi_22_0417@yahoo.co.jp>
2023-05-02 19:42:30 +0900

    更新

--- main.cc
+++ main.cc
@@ -1,7 +1,26 @@
 #include <iostream>
 
+enum Day {
+    Sun = 1,  // 1
+    Mon,      // 2
+    Tue,      // 3
+    Wed = 8,  // 8
+    Thu,      // 9
+    Fri,      // 10
+    Sat       // 11
+};
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    Day day = Fri;
+    std::cout << day << std::endl;
+
+    int x = 5;
+    int* p = &x;
+    int y = *p;
+
+    std::cout << y << std::endl;
 
     return 0;
 }
