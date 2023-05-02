55352029ebe13d84c047da9ec072b94844bfaca9
naanahi <ryuu_yoshi_22_0417@yahoo.co.jp>
2023-05-02 20:23:54 +0900

    更新

--- main.cc
+++ main.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 
 void HelloWorld(int n);  // 前方宣言
+int Add(int x, int y);
 
 enum Day {
     Sun = 1,  // 1
@@ -12,7 +13,7 @@ enum Day {
     Sat       // 11
 };
 
-int main() {
+int main(int argc, char* argv[]) {
     std::cout << "Hello, World!" << std::endl;
 
     /* リテラル */
@@ -37,7 +38,13 @@ int main() {
         }
     }
 
+    y = 2;
     HelloWorld(y);
+
+    // int (*fp)(int, int) = Add;  // 関数 Add のアドレスを保持する関数ポインタ fp
+    auto fp = Add;
+    int result = fp(3, 5);      // 関数ポインタ fp を介して関数 Add が実行される
+    std::cout << result << std::endl;  // 8
 
     return 0;
 }
@@ -46,4 +53,8 @@ void HelloWorld(int n) {
     for (int i = 0; i < n; ++i) {
         std::cout << "[" << i << "] " << "Hello World!" << std::endl;
     }
+}
+
+int Add(int x, int y) {
+    return x + y;
 }
