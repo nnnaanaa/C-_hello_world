8cbfd9503c4990cbaadb93d6faa232745fc3755d
naanahi <ryuu_yoshi_22_0417@yahoo.co.jp>
2023-05-03 11:12:04 +0900

    ほよ

--- main.cc
+++ main.cc
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <array>
 
 void HelloWorld(int n);  // 前方宣言
 int Add(int x, int y);
+void PrintArray(const int* x, std::size_t num);
 
 enum Day {
     Sun = 1,  // 1
@@ -14,24 +16,24 @@ enum Day {
 };
 
 int main(int argc, char* argv[]) {
-    std::cout << "Hello, World!" << std::endl;
+    // std::cout << "Hello, World!" << std::endl;
 
     /* リテラル */
     Day day = Fri;
-    std::cout << day << std::endl;
+    //std::cout << day << std::endl;
 
     /* ポインタの使い方 */
     int x = 20;
     int* p = &x;
     int y = *p;
 
-    std::cout << y << std::endl;
+    // std::cout << y << std::endl;
 
     /* 繰り返し処理 */
     bool done = false;
     while (!done) {
         x += x - 3;
-        std::cout << x << std::endl;
+        // std::cout << x << std::endl;
 
         if (x % 5 == 0) {
             done = true;
@@ -44,17 +46,40 @@ int main(int argc, char* argv[]) {
     // int (*fp)(int, int) = Add;  // 関数 Add のアドレスを保持する関数ポインタ fp
     auto fp = Add;
     int result = fp(3, 5);      // 関数ポインタ fp を介して関数 Add が実行される
-    std::cout << result << std::endl;  // 8
+    // std::cout << result << std::endl;  // 8
+
+    int xl[] = { 2, 4, 6, 8, 10 };
+    int* pp = xl;
+    std::cout << *pp << std::endl;        // 2
+    std::cout << *(pp + 1) << std::endl;  // 4
+    std::cout << *(pp + 2) << std::endl;  // 6
+    std::cout << *(pp + 3) << std::endl;  // 8
+    std::cout << *(pp + 4) << std::endl;  // 10
+
+    std::array<int, 3> arr2_1{};
+    std::array<int, 3> arr2_2 = arr2_1;//OK
+
+    std::cout << arr2_2[0] << std::endl;
+
+    std::string s = "Hello";
+    std::cout << s.size() << std::endl;   // 5
+
 
     return 0;
 }
 
 void HelloWorld(int n) {
     for (int i = 0; i < n; ++i) {
-        std::cout << "[" << i << "] " << "Hello World!" << std::endl;
+        // std::cout << "[" << i << "] " << "Hello World!" << std::endl;
     }
 }
 
 int Add(int x, int y) {
     return x + y;
+}
+
+void PrintArray(const int* x, std::size_t num) {
+    for (std::size_t i = 0; i < num; ++i) {
+        std::cout << x[i] << std::endl;
+    }
 }
