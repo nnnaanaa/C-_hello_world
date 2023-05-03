16e1db29747d85dffbe302ed6f24ad7f514461ad
naanahi <ryuu_yoshi_22_0417@yahoo.co.jp>
2023-05-03 12:16:20 +0900

    いろいろコメントアウト

--- main.cc
+++ main.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <array>
+#include <vector>
 
 void HelloWorld(int n);  // 前方宣言
 int Add(int x, int y);
@@ -16,54 +17,59 @@ enum Day {
 };
 
 int main(int argc, char* argv[]) {
-    // std::cout << "Hello, World!" << std::endl;
+    //// std::cout << "Hello, World!" << std::endl;
 
-    /* リテラル */
-    Day day = Fri;
-    //std::cout << day << std::endl;
+    ///* リテラル */
+    //Day day = Fri;
+    ////std::cout << day << std::endl;
 
-    /* ポインタの使い方 */
-    int x = 20;
-    int* p = &x;
-    int y = *p;
+    ///* ポインタの使い方 */
+    //int x = 20;
+    //int* p = &x;
+    //int y = *p;
 
-    // std::cout << y << std::endl;
+    //// std::cout << y << std::endl;
 
-    /* 繰り返し処理 */
-    bool done = false;
-    while (!done) {
-        x += x - 3;
-        // std::cout << x << std::endl;
+    ///* 繰り返し処理 */
+    //bool done = false;
+    //while (!done) {
+    //    x += x - 3;
+    //    // std::cout << x << std::endl;
 
-        if (x % 5 == 0) {
-            done = true;
-        }
+    //    if (x % 5 == 0) {
+    //        done = true;
+    //    }
+    //}
+
+    //y = 2;
+    //HelloWorld(y);
+
+    //// int (*fp)(int, int) = Add;  // 関数 Add のアドレスを保持する関数ポインタ fp
+    //auto fp = Add;
+    //int result = fp(3, 5);      // 関数ポインタ fp を介して関数 Add が実行される
+    //// std::cout << result << std::endl;  // 8
+
+    //int xl[] = { 2, 4, 6, 8, 10 };
+    //int* pp = xl;
+    //std::cout << *pp << std::endl;        // 2
+    //std::cout << *(pp + 1) << std::endl;  // 4
+    //std::cout << *(pp + 2) << std::endl;  // 6
+    //std::cout << *(pp + 3) << std::endl;  // 8
+    //std::cout << *(pp + 4) << std::endl;  // 10
+
+    //std::array<int, 3> arr2_1{};
+    //std::array<int, 3> arr2_2 = arr2_1;//OK
+
+    //std::cout << arr2_2[0] << std::endl;
+
+    //std::string s = "Hello";
+    //std::cout << s.size() << std::endl;   // 5
+
+
+    std::vector<int> x = { 0, 1, 2, 3, 4 };
+    for (auto it = x.begin(); it != x.end(); ++it) {
+        std::cout << *it << std::endl;
     }
-
-    y = 2;
-    HelloWorld(y);
-
-    // int (*fp)(int, int) = Add;  // 関数 Add のアドレスを保持する関数ポインタ fp
-    auto fp = Add;
-    int result = fp(3, 5);      // 関数ポインタ fp を介して関数 Add が実行される
-    // std::cout << result << std::endl;  // 8
-
-    int xl[] = { 2, 4, 6, 8, 10 };
-    int* pp = xl;
-    std::cout << *pp << std::endl;        // 2
-    std::cout << *(pp + 1) << std::endl;  // 4
-    std::cout << *(pp + 2) << std::endl;  // 6
-    std::cout << *(pp + 3) << std::endl;  // 8
-    std::cout << *(pp + 4) << std::endl;  // 10
-
-    std::array<int, 3> arr2_1{};
-    std::array<int, 3> arr2_2 = arr2_1;//OK
-
-    std::cout << arr2_2[0] << std::endl;
-
-    std::string s = "Hello";
-    std::cout << s.size() << std::endl;   // 5
-
 
     return 0;
 }
